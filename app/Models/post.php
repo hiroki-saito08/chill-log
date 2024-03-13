200205af8bd31d90f7fb42259292421f07deed44
saitohiroki <hirokisaito0813@gmail.com>
2024-03-13 13:13:44 +1100

    model, seeder基本設定

--- app/Models/post.php
+++ app/Models/post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'status',
+        'title',
+        'content',
+        'image_id'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ChillControllers\IndexController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', [IndexController::class, 'index']);
+use App\Http\Controllers\ChillControllers\PostController;
+use App\Http\Controllers\ChillControllers\ReviewController;
+use App\Http\Controllers\ChillControllers\FavoriteController;
+use App\Http\Controllers\ChillControllers\ImageController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -27,6 +18,11 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+// Route::resource('/', IndexController::class);
+Route::resource('post', PostController::class)->middleware(['auth', 'verified']);
+Route::resource('review', ReviewController::class)->middleware(['auth', 'verified']);
+Route::resource('favorite', FavoriteController::class)->middleware(['auth', 'verified']);
+Route::resource('image', ImageController::class)->middleware(['auth', 'verified']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
