6d3b28508f9e4822f708fecb415e62e680f8ef77
saitohiroki <hirokisaito0813@gmail.com>
2024-07-29 15:05:20 +1000

    画像アップロード時に横幅1000にリサイズ

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -18,42 +18,42 @@ class ImageSeeder extends Seeder
         DB::table('images')->insert([
             [
                 'name' => 'P1030094.JPG',
-                'path' => '/storage/images/postImages/P1030269.JPG',
+                'path' => '/storage/images/postImages/P1020857.JPG',
                 'post_id' => 1,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'name' => 'P1020947.JPG',
-                'path' => '/storage/images/postImages/P1020981.JPG',
+                'path' => '/storage/images/postImages/P1020858.JPG',
                 'post_id' => 2,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'name' => '20170617_094344.jpg',
-                'path' => '/storage/images/postImages/1711602685_P1020858.JPG',
+                'path' => '/storage/images/postImages/P1020867.JPG',
                 'post_id' => 3,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'name' => '20170610_190502.jpg',
-                'path' => '/storage/images/postImages/20170617_112110.jpg',
+                'path' => '/storage/images/postImages/P1020963.jpg',
                 'post_id' => 4,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'name' => '20170604_122112.jpg',
-                'path' => '/storage/images/postImages/20170604_122112.jpg',
+                'path' => '/storage/images/postImages/P1030069.jpg',
                 'post_id' => 5,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'name' => '20170602_145718-1.jpg',
-                'path' => '/storage/images/postImages/P1030161.JPG',
+                'path' => '/storage/images/postImages/P1030096.JPG',
                 'post_id' => 6,
                 'created_at' => now(),
                 'updated_at' => now()
