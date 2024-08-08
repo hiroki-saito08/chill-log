82e0a3d65634763260bce1a1097f96af6b3fb152
saitohiroki <hirokisaito0813@gmail.com>
2024-08-08 13:26:53 +1000

    レイアウト修正

--- resources/js/Components/Search.vue
+++ resources/js/Components/Search.vue
@@ -18,7 +18,7 @@ const sendSearch = () => {
 <template>
   <!-- 検索バー -->
   <div class="sm-search-area">
-    <div class="sm-search-area-inner mx-auto md:flex md:flex-wrap justify-around h-14 md:w-3/5">
+    <div class="sm-search-area-inner mx-auto md:flex md:flex-wrap justify-around md:h-14 md:w-3/5">
       <input type="text" id="searchWord" v-model="searchWord" name="searchWord" class="mx-auto w-3/5 block bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
       <button @click="sendSearch" class="mx-auto block text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">検索</button>
     </div>
