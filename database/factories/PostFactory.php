08627f26da8f0d5f7c84650dcf18c73fa7209073
saitohiroki <hirokisaito0813@gmail.com>
2025-05-07 17:37:41 +1000

    testdata追加 微修正

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,21 +3,26 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Post;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\post>
  */
 class PostFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(), // 紐づけ
+            'title' => $this->faker->sentence(),
+            'category' => $this->faker->randomElement(['Park', 'Cafe', 'Beach']),
+            'location_name' => $this->faker->address(),
+            'latitude' => $this->faker->latitude(),
+            'longitude' => $this->faker->longitude(),
+            'description' => $this->faker->paragraph(),
+            'visit_time' => $this->faker->randomElement(['morning', 'afternoon', 'evening', 'night']),
+            'status' => 'public',
         ];
     }
 }
